ef43c704d8b45b81680f7fe729bc9b79b011cf99
TeterinMN <Scorpion.TMN@ya.ru>
2023-04-04 20:36:33 +0900

    Написал код ко всем задачам задания

--- Task_64/Program.cs
+++ Task_64/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// Функция вывода натуральных чисел
+static void PrintNaturalNum(int n)
+{
+    if (n <= 0)
+    {
+        return;
+    }
+    Console.Write(n + ", ");
+    PrintNaturalNum(n - 1);
+}
+
+Console.Clear();
+// Ввод числа
+Console.Write("Введите число: >> ");
+int n = int.Parse(Console.ReadLine()!);
+
+// Вывод
+PrintNaturalNum(n);
--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вычисление функции Аккермана
+static int Ackerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return Ackerman(m - 1, 1);
+    }
+    else
+    {
+        return Ackerman(m - 1, Ackerman(m, n - 1));
+    }
+}
+
+// Ввод данных
+Console.Clear();
+Console.Write("Введите M:>> ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите N:>> ");
+int n = int.Parse(Console.ReadLine()!);
+
+// Вычисление и вывод
+int result = Ackerman(m, n);
+Console.WriteLine("Значение функции Аккермана для m={0} и n={1} равно {2}", m, n, result);
