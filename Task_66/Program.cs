e0e221ea9c0548fd4a93264086b08aed318267fb
TeterinMN <Scorpion.TMN@ya.ru>
2023-04-05 11:41:42 +0900

    Переписал код на использование рекурсии

--- Task_66/Program.cs
+++ Task_66/Program.cs
@@ -1,12 +1,40 @@
-﻿// Функция нахождения суммы элементов от М до N
+﻿// // Функция нахождения суммы элементов от М до N
+// static int Result(int m, int n)
+// {
+//     int sum = 0;
+//     for (int i = m; i <= n; i++)
+//     {
+//         sum += i;
+//     }
+//     return sum;
+// }
+
+// // Ввод значений
+// Console.Clear();
+// Console.Write("Введите M:>> ");
+// int m = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите N:>> ");
+// int n = int.Parse(Console.ReadLine()!);
+
+// // Вычисление и вывод
+// int sum = Result(m, n);
+// Console.WriteLine();
+// Console.WriteLine("Сумма натуральных чисел в промежутке от {0} до {1} равна {2}", m, n, sum);
+
 static int Result(int m, int n)
 {
-    int sum = 0;
-    for (int i = m; i <= n; i++)
+    if (m == n)
     {
-        sum += i;
+        return m;
     }
-    return sum;
+    else if (m > n)
+    {
+        return 0;
+    }
+    else
+    {
+        return m + Result(m + 1, n);
+    }
 }
 
 // Ввод значений
